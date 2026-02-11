af03feda5a63872c5d52ddf62108ef85931e643f
ansdl <ansdlsgur0123@gmail.com>
2026-02-11 10:51:17 +0900

    DivideTwoNumbers 코드카타 2-2 AgeCalculator 코드카아 2-3

---
+++ src/main/java/com/sparta/AgeCalculator.java
@@ -0,0 +1,7 @@
+package com.sparta;
+
+public class AgeCalculator {
+    public int solution(int age) {
+        return 2022 - (age - 1);
+    }
+}
---
+++ src/main/java/com/sparta/DivideTwoNumbers.java
@@ -0,0 +1,7 @@
+package com.sparta;
+
+public class DivideTwoNumbers {
+    public int main(int num1, int num2) {
+        return num1 / num2;
+    }
+}
